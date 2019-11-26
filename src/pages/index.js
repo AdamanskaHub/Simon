388af7d7c6d4a117b04aa6536cb557e997b50f24
Emilie G <adamanska@gmail.com>
2019-11-26 18:08:56 +0100

    first section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,34 @@
 import React from "react"
 import { Link } from "gatsby"
+import "../sass/main.scss"
 
 // import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <>
+
+  <div className="container">
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    
+    <section className="intro">
+      <div className="blob"></div>
+      <h1>At the tip of a thought</h1>
+      <p>Some text</p>
+    </section>
+    
     {/* <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div> */}
-    <Link to="/page-2/">Go to page 2</Link>
-  </>
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
+
+    <section className="goal">
+      <h2>My goal</h2>
+      <p>my goals p</p>
+    </section>
+  </div>
 )
 
 export default IndexPage
